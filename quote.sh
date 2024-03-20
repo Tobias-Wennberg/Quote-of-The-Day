98f2e6e7304d8adf39e84478b11657a2e1ba7aa2
Tobias Wennberg <21towe@stockholmscience.se>
2024-03-20 11:20:39 +0100

    big update, put quote lists in the quotelist directory, in a file with the authors name

--- quote.sh
+++ quote.sh
@@ -6,9 +6,21 @@ if [[ $sleep_status != *"sleep"* ]]; then
   echo "The computer is in sleep mode. Exiting the script."
   exit
 fi
+lists=$(find quotelist/ -maxdepth 1 -type f -print0 | tr '\0' '\n' | awk -F '/' '{print $NF}')
+list_len=$(($(echo $lists | wc -l)+1))
+echo lists:
+echo $lists
+echo len
+echo $list_len
+
+list_num=$((($(echo $RANDOM) % $list_len)+1))
+list_file_name=$(echo "$lists" | awk "NR==$list_num")
+quote_path="/$HOME/.config/quoteOfTheDay/quotelist/$list_file_name"
+name=$list_file_name
 
 
-
+<<<<<<< HEAD
+=======
 if [[ $(($(echo $RANDOM) % 2)) == "1" ]]; then
 	quote_path="/$HOME/.config/quoteOfTheDay/quotes_mao.txt"
 	name="Mao Zedong"
@@ -17,6 +29,7 @@ else
 	name="Theodore John Kaczynski"
 fi
 
+>>>>>>> b3144ff6ac0465f9fbdd844d64786151a43cf1c5
 declare -a quotes
 while IFS= read -r line; do
 	quotes+=("$line")
@@ -24,6 +37,6 @@ done < "$quote_path"
 
 quote_length=${#quotes[@]}
 
-index=$((RANDOM % quote_length))
+index=$(($RANDOM % quote_length))
 
 notify-send -t 0 "Quote Of the Day" "\"${quotes[index]}\" - ${name}"
